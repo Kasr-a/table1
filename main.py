3cfa76ed4660d98f560c4596d83d02a936fb7d86
KasraNarimani <kasra@brickeye.com>
2022-10-13 11:01:30 -0400

    Added Table2, Fixed the graph from randomly rearranging numbers, formatted into columns

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title("Data table")
+st.title("Data table attempt #1")
 
-
+@st.cache(allow_output_mutation=True)
 def fetch_data():
 
     return pd.DataFrame(
@@ -12,11 +12,18 @@ def fetch_data():
     )
 
 
-data = fetch_data()
 "Choose the box you want to edit by choosing its row and column number"
-row = st.number_input('row', max_value=data.shape[0])
-col = st.number_input('column', max_value=data.shape[1])
-"Input the new value"
-val = st.number_input('value')
-data.values[row][col] = val
+col1, col2 = st.columns(2, gap="small")
+col3, valcol = st.columns(2, gap="small")
+data = fetch_data()
+
+with col1:
+    row = st.number_input('row', max_value=data.shape[0])
+with col2:
+    col = st.number_input('column', max_value=data.shape[1])
+with col3:
+    st.markdown("Input the new value")
+with valcol:
+    val = st.number_input('value')
+data.loc[row][col] = val
 st.dataframe(data)
