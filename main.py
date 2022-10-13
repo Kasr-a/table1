1b894ffe626f6eec542cc153786ee45fd73c02f0
KasraNarimani <kasra@brickeye.com>
2022-10-13 10:17:14 -0400

    Made the code start working

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title("Data table")
-data = pd.DataFrame(
-    np.random.randn(50, 20),
-    columns=('col %d' % i for i in range(20))
-)
-val = st.number_input('value')
+
+
+def fetch_data():
+
+    return pd.DataFrame(
+        np.random.randn(50, 20),
+        columns=('col %d' % i for i in range(20))
+    )
+
+
+data = fetch_data()
+"Choose the box you want to edit by choosing its row and column number"
 row = st.number_input('row', max_value=data.shape[0])
 col = st.number_input('column', max_value=data.shape[1])
+"Input the new value"
+val = st.number_input('value')
 data.values[row][col] = val
-st.dataframe[data]
+st.dataframe(data)
