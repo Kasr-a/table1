4dcb50285e0577b160d089634b4c1a51cc992356
KasraNarimani <kasra@brickeye.com>
2022-10-19 09:34:25 -0400

    Attempt #4 technically successful, probably isnt usable because of dropbox compression

---
+++ pages/Table4.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.set_page_config(layout='wide')
+
+
+@st.cache(allow_output_mutation=True)
+def fetch1():
+
+    return pd.DataFrame(
+        np.random.randn(20, 50),
+        columns=('col %d' % i for i in range(50))
+    )
+
+
+df = fetch1()
+colNum = len(df.columns)
+colsData = {}
+for curCol in range(0, colNum):
+    colsData[f'{curCol}'] = df[f'col {curCol}'].tolist()
+
+col = st.columns(colNum, gap='small')
+index = 0
+
+for colN in range(0, colNum):
+    with col[colN]:
+        for data in colsData.values():
+            st.selectbox("Grid", data, label_visibility='collapsed', key=f'column {index}')
+            index += 1
