a8cc6b1e2b6b9e223606a2c6613a4cf1d3783220
KasraNarimani <kasra@brickeye.com>
2022-10-26 10:38:07 -0400

    Finished adding 'rows' to keep table in line with itself

--- pages/Table4.py
+++ pages/Table4.py
@@ -7,34 +7,34 @@ st.set_page_config(layout='wide')
 def fetch1():
     return pd.DataFrame({
         0: [
-            'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry', 'tom', 'jerry', 'diablo', 'henry'
+            'tom', 'jerry', 'diablo', 'henry'
         ],
         1: [
-            20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129, 20, 30, 50, 129
+            20, 30, 50, 129
         ],
         2: [
-            'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint', 'famished', 'ecstatic', 'neurological', 'faint'
+            'famished', 'ecstatic', 'neurological', 'faint'
         ],
         3: [
-            12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121, 12, 23, 235, 121
+            12, 23, 235, 121
         ],
         4: [
-            'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute',  'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute', 'sport', 'lawyer', 'represent', 'cute'
+            'sport', 'lawyer', 'represent', 'cute'
         ],
         5: [
-            44, 37, 86, 77, 44, 37, 86, 77, 44, 37, 86, 77, 44, 37, 86, 77,  44, 37, 86, 77, 44, 37, 86, 77, 44, 37, 86, 77, 44, 37, 86, 77, 44, 37, 86, 77, 44, 37, 86, 77, 44, 37, 86, 77, 44, 37, 86, 77,  44, 37, 86, 77, 44, 37, 86, 77, 44, 37, 86, 77, 44, 37, 86, 77
+            44, 37, 86, 77
         ],
         6: [
-            'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip', 'pig', 'define', 'variable', 'sip'
+            'pig', 'define', 'variable', 'sip'
         ],
         7: [
-            26, 93, 27, 32, 26, 93, 27, 32, 26, 93, 27, 32, 26, 93, 27, 32,  26, 93, 27, 32, 26, 93, 27, 32, 26, 93, 27, 32, 26, 93, 27, 32, 26, 93, 27, 32, 26, 93, 27, 32, 26, 93, 27, 32, 26, 93, 27, 32,  26, 93, 27, 32, 26, 93, 27, 32, 26, 93, 27, 32, 26, 93, 27, 32
+            26, 93, 27, 32
         ],
         8: [
-            'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus', 'grounds', 'fee', 'option', 'bus'
+            'grounds', 'fee', 'option', 'bus'
         ],
         9: [
-            'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46', 'output34', 'introduction21', 'press50', 'penalty46'
+            'output34', 'introduction21', 'press50', 'penalty46'
         ]
     })
 
@@ -57,44 +57,59 @@ def tbl_data():
 def db_table():
     df = fetch1()
     colNum = len(df.columns)
+    rowNum = len(df.index)
     tblFTE = tbl_data()
     colsData = {}
+    rows = [st.container()]
+
+
     for curCol in range(0, colNum):
         colsData[f'{curCol}'] = df[curCol].tolist()
+    for curRow in range(0, rowNum):
+        rows.append(st.container())
 
-    col = st.columns(colNum, gap='small')
+
     index = 0
 
-    for colN in range(0, colNum):
-        flip = 0
-        flip2 = 10
-        with col[colN]:
-            st.title(f'col {colN + 1}')
-            for key in range(0, len(colsData[str(colN)])):
-                if tblFTE[str(colN)] == 'dropbox':
-                    st.selectbox('Grid', colsData[str(colN)], label_visibility='collapsed', key=f'datapoint {index}')
-                    if flip2 > 0:
-                        st.markdown('##')
-                        st.markdown('')
-                        flip2 -= 1
-                    else:
-                        st.markdown('#')
-                        flip2 = 10
+    with rows[0]:
+        cols = st.columns(colNum, gap='small')
+        for colN in range(0, colNum):
+            with cols[colN]:
+                st.title(f'col {colN + 1}')
+    flip = 0
+    flip2 = 10
+    for rowN in range(0, rowNum):
 
-                    index += 1
-                if tblFTE[str(colN)] == 'text':
-                    st.text(colsData[str(colN)][key])
-                    st.markdown('*****')
+        with rows[rowN+1]:
+            cols = st.columns(colNum, gap='small')
+            for colN in range(0, colNum):
 
-                    if flip == 1:
-                        st.markdown('')
-                        flip = 0
-                    else:
-                        flip = 0
-                    index += 1
-                if tblFTE[str(colN)] == 'mult':
-                    st.multiselect('Grid', colsData[str(colN)], label_visibility='collapsed', key=f'datapoint {index}')
-                    index += 1
+                with cols[colN]:
+
+                    if tblFTE[str(colN)] == 'dropbox':
+                        st.selectbox('Grid', colsData[str(colN)], label_visibility='collapsed', key=f'datapoint {index}')
+                        if flip2 > 0:
+                            st.markdown('##')
+                            st.markdown('')
+                            flip2 -= 1
+                        else:
+                            st.markdown('#')
+                            flip2 = 10
+
+                        index += 1
+                    if tblFTE[str(colN)] == 'text':
+                        st.text(colsData[str(colN)][rowN])
+                        st.markdown('*****')
+
+                        if flip == 1:
+                            st.markdown('')
+                            flip = 0
+                        else:
+                            flip = 0
+                        index += 1
+                    if tblFTE[str(colN)] == 'mult':
+                        st.multiselect('Grid', colsData[str(colN)], label_visibility='collapsed', key=f'datapoint {index}')
+                        index += 1
 
 
 
